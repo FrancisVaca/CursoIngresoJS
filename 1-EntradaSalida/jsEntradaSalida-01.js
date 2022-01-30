d2ecdd17ab77121febaefee876f0551993cc17a5
FrancisVaca <98625530+FrancisVaca@users.noreply.github.com>
2022-01-29 22:36:15 -0300

    Add files via upload
    ultimas actualizaciones

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -3,6 +3,7 @@ Al presionar el  botón,
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-	alert("Bienvenidos A Bordo");
+	alert("Bienvenidos A Bordo a todos");
+	alert("Chau a todos, Buenas noches");
 }
 

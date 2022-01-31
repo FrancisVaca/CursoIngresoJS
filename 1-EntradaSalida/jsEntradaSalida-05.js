f25dbaba0a437f21b4fc6f24df31629c3ff27933
FrancisVaca <98625530+FrancisVaca@users.noreply.github.com>
2022-01-30 23:48:07 -0300

    Add files via upload
    nuevas modificaciones

--- 1-EntradaSalida/jsEntradaSalida-05.js
+++ 1-EntradaSalida/jsEntradaSalida-05.js
@@ -2,7 +2,26 @@
 Debemos lograr tomar nombre y edad por ID y mostrarlos concatenados 
 ej.: "Usted se llama José y tiene 66 años" 	*/
 function mostrar()
-{	
-	alert("ok");
+    //creo la variables para cada campo
+
+{	let nombre;
+	let edad;
+  
+	//pongo la id del html de donde va a ir el valor de la variable en la caja de texto
+
+	nombre = document.getElementById("txtIdNombre").value;
+
+	edad = document.getElementById("txtIdEdad").value;
+
+	//Aqui combino en la alerta la cadena de texto mas los datos de la variable utilizndo el +
+	
+
+	//alert("Usted se llama " + nombre + " y tiene " + edad + " años");
+
+	alert(`Usted se llama ${nombre} y tiene ${edad} años`)
+
+
+
+
 }
 
--- 1-EntradaSalida/jsEntradaSalida-06.js
+++ 1-EntradaSalida/jsEntradaSalida-06.js
@@ -4,6 +4,17 @@ transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
-	alert("ok");
-}
+	let n1;
+    let n2;
+    let res;
 
+	n1 = parseInt(document.getElementById("txtIdNumeroUno").value);
+
+    n2 = parseInt(document.getElementById("txtIdNumeroDos").value);
+
+	res = n1 + n2;
+
+	alert("El resultado es " + res );
+
+	
+}
